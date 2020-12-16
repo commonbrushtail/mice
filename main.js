598fc4a672a46457ef7182dab77cbb1040ad5662
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-12-16 12:42:02 +0700

    adssad
    asd

--- main.js
+++ main.js
@@ -3,27 +3,6 @@ let worldShadow = document.querySelector(".worldShadow"),
     contentBox = document.querySelector(".contentBox"),
     delta = MotionPathPlugin.getRelativePosition(contentBox, worldShadow, [0.5, 0.5], [0.5, 0.5]);
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    ScrollTrigger.refresh();
-
-gsap.set(contentBox, {
-    x: "+=" + 50% + delta.x,
-    y: "+=" + delta.y,
-    duration: 2,
-    ease: "power2.inOut"
-  });
-    });    
-window.addEventListener('onResize', (event) => {
-    ScrollTrigger.refresh();
-
-gsap.set(contentBox, {
-    x: "+=" + 50% + delta.x,
-    y: "+=" + delta.y,
-    duration: 2,
-    ease: "power2.inOut"
-});
-    
-});    
 
 
 
