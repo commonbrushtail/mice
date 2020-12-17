091f78f00af031f3de6ded5459c4ee41d9bc7395
commonbrushtail <commonbrushtail@gmail.com>
2020-12-17 17:58:55 +0700

    asdas
    asdasd

--- main.js
+++ main.js
@@ -18,29 +18,20 @@ function findPosition(){
         
 }
 
-
-
-
-
-
 function setPosition(){
     gsap.set('.contentBox1',{
+        immediateRender:true,
         y: worldShadowTop 
     })
     gsap.set('.contentBox2',{
+        immediateRender:true,
         y: travellerShadowTop 
     })
     gsap.set('.contentBox3',{
+        immediateRender:true,
         y: flagShadowShadowTop 
     })
 }
-
-
-
-
-
-
-
 
 window.addEventListener('load', ()=>{
     findPosition()
@@ -52,16 +43,118 @@ window.addEventListener('resize',()=>{
 });
 
 
-    var action = gsap.timeline({defaults: {duration: 1},
-    scrollTrigger: {
-      trigger: ".svgLine",
-      scrub: true,
-      start: "top center",
-      end: "bottom center",
-      markers:true,
-    }})
 
-  .from("#pathLine", {drawSVG: 0}, 0)
-  .to(".pathBall", {motionPath: {path: "#pathLine", alignOrigin: [0.5, 0.5]}}, 0)
+document.addEventListener('DOMContentLoaded', () => {
+    findPosition()
+    setPosition()
+    
+    
+    
+    
+    
+    
+    gsap.set('.pathBall',{
+        motionPath:{
+            path:'.pathLine',
+            align:'.pathLine',
+            alignOrigin: [0.5, 0.5],
+            start:0,
+            end:0,
+            
+        }
+    })
+    
+    
+    
+    
+    var tl = gsap.timeline({
+        defaults:{
+            duration:0.0001
+        },
+        
+        scrollTrigger:{
+            markers:true,
+            trigger:'.contentBox1',
+            start: '100+top bottom',
+            end:'-500top top',
+            scrub:true,
+            endTrigger: ".section2",
+        }
+    })
+    
+    tl.to('.pathBall',{
+    
+        motionPath:{
+            path:'.pathLine',
+            align:'.pathLine',
+            alignOrigin: [0.5, 0.5],
+        }
+    })
+    tl.from('.pathLine',{
+        drawSVG: 0
+    },"<")
+    
+   
 
-  
+    tl.fromTo('.worldShadow',{
+        opacity:0
+    },{
+        opacity:'100%',
+        scrollTrigger:{
+            markers:true,
+            trigger:'.contentBox1',
+            start: 'bottom bottom',
+            end:'top top',
+            scrub:true,
+            endTrigger: ".contentBox2",
+        }
+    },"<")
+
+    tl.fromTo('.travellerShadow',{
+        opacity:0
+    },{
+        opacity:'100%',
+        scrollTrigger:{
+            markers:true,
+            trigger:'.contentBox2',
+            start: 'bottom bottom',
+            end:'-120+top top',
+            scrub:true,
+            endTrigger: ".contentBox3",
+        }
+    },"<")
+
+    tl.fromTo('.flagShadow',{
+        opacity:0
+    },{
+        opacity:'100%',
+        scrollTrigger:{
+            markers:true,
+            trigger:'.contentBox3',
+            start: 'bottom bottom',
+            end:'top -450+top',
+            scrub:true,
+            endTrigger: ".contentBox3",
+        }
+    },"<")
+    
+    tl.to('.pathBall',{
+        scale:10,
+        fill:'white',
+        scrollTrigger:{
+            trigger:'.section2',
+            start:"top bottom",
+            end:"bottom bottom",
+            scrub:true,
+            markers:true,
+        }
+    })
+    
+
+})
+
+
+
+
+
+
