d2ae890ee0024a05804e02648313fc94409acef3
commonbrushtail <commonbrushtail@gmail.com>
2020-12-21 17:58:02 +0700

    asdas
    adsad

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-gsap.registerPlugin(MotionPathPlugin, ScrollTrigger, DrawSVGPlugin,MorphSVGPlugin);
+gsap.registerPlugin(MotionPathPlugin, ScrollTrigger, DrawSVGPlugin,MorphSVGPlugin,CSSRulePlugin);
 
-
+var containerBefore = CSSRulePlugin.getRule(".container:before"); //get the rule
+var section2Before = CSSRulePlugin.getRule(".section2:before"); //get the rule
+var section2After = CSSRulePlugin.getRule(".section2:after"); //get the rule
 var worldShadow = document.querySelector(".worldShadow"),
     travellerShadow = document.querySelector(".travellerShadow"),
     flagShadow = document.querySelector(".flagShadow"),
@@ -51,6 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     
     MorphSVGPlugin.convertToPath('.ball02') 
+    MorphSVGPlugin.convertToPath('.pathBall') 
     
 
 
@@ -72,24 +75,35 @@ document.addEventListener('DOMContentLoaded', () => {
           duration:5,
           
         }})
-    .to('.ball02',{morphSVG:'.decoration1',x:-190,y:-2016,fill:"url(#linear-gradient2"})   
+    .to('.ball02',{morphSVG:'.decoration1',x:-190,y:-2016,}) 
       
     
     
     
     
     var tl = gsap.timeline({
+        onComplete:()=>{
+            gsap.to('.section2Ball',{
+                scale:1,
+                backgroundColor:'white',
+                
+                
+            },""
+                )
+        
+        },
         defaults:{
             duration:30
         },
         
         scrollTrigger:{
-            markers:true,
+        
             trigger:'.contentBox1',
             start: '100+top bottom',
-            end:'-500top top',
+            end:'-100top top',
             scrub:true,
-            endTrigger: ".section2",
+            endTrigger:".section2",
+            markers:true,
         }
     })
     
@@ -101,6 +115,7 @@ document.addEventListener('DOMContentLoaded', () => {
             alignOrigin: [0.5, 0.5],
         }
     })
+    
     tl.from('.pathLine',{
         drawSVG: 0
     },"<")
@@ -112,7 +127,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },{
         opacity:'100%',
         scrollTrigger:{
-            markers:true,
+        
             trigger:'.contentBox1',
             start: 'bottom bottom',
             end:'top top',
@@ -126,7 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },{
         opacity:'100%',
         scrollTrigger:{
-            markers:true,
+           
             trigger:'.contentBox2',
             start: 'bottom bottom',
             end:'-120+top top',
@@ -140,7 +155,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },{
         opacity:'100%',
         scrollTrigger:{
-            markers:true,
+       
             trigger:'.contentBox3',
             start: 'bottom bottom',
             end:'top -450+top',
@@ -149,22 +164,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     },"<")
 
+   
+ 
+   
+    
     tl.add(action,"-=4.5")
 
+    
+    
 
-    /*
-    tl.to('.pathBall',{
-        scale:10,
-        fill:'white',
-        scrollTrigger:{
-            trigger:'.section2',
-            start:"top bottom",
-            end:"bottom bottom",
-            scrub:true,
-            markers:true,
-        }
-    })
-    */
+    
+    
+    
   
    
 
@@ -172,9 +183,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+gsap.fromTo(containerBefore,
+    {
+    background: "rgb(0,255,194)",
+    background: "linear-gradient(0deg, rgba(0,255,194,1) 0%, rgba(4,246,195,1) 12%, rgba(17,223,201,1) 24%, rgba(37,186,209,1) 39%, rgba(66,134,222,1) 59%, rgba(102,69,237,1) 80%, rgba(137,6,252,1) 100%)",
+    repeat:-1,
+ 
+    
+},
+    {
+        background: "rgb(0,255,194)",
+        background: "linear-gradient(360deg, rgba(0,255,194,1) 0%, rgba(4,246,195,1) 12%, rgba(17,223,201,1) 24%, rgba(37,186,209,1) 39%, rgba(66,134,222,1) 59%, rgba(102,69,237,1) 80%, rgba(137,6,252,1) 100%)",
+        repeat:-1,
+        duration:6,
+        ease: "none"
+    }
 
+);
 
+/*
+MotionPathHelper.create(".pathBall",{
+  path: ".pathLine",
+  pathWidth: 5,
+  pathColor: "red",
+  pathOpacity: 0.6,
+  selected: true,
+  start: 0.1,
+  end: 1,
+  duration: 5,
+  ease: "power2.inOut"
+})
 
+*/
 
 
 
